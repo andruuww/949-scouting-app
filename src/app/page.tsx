7ee0943c88875e3828bd18c335926ccaf325e6e4
evan <evan@evang.dev>
2023-08-19 07:06:02 +0200

    qol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,24 @@ import MenuBar from '@/components/menu-bar';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
     const router = useRouter();
     const [name, setName] = useState('');
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const [alreadyLoggedInAs, setAlreadyLoggedInAs] = useState<null | string>(
+        null
+    );
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setHasLoaded(true);
+            if (localStorage.getItem('scoutName')) {
+                setAlreadyLoggedInAs(localStorage.getItem('scoutName')!);
+            }
+        }
+    }, [typeof window]);
 
     const onSubmit = () => {
         if (typeof window !== 'undefined') {
@@ -28,9 +41,24 @@ export default function Home() {
                     onChange={(e) => setName(e.target.value)}
                 />
 
-                <Button type="submit" className="mt-3" onClick={onSubmit}>
+                <Button
+                    type="submit"
+                    className="mt-3"
+                    onClick={onSubmit}
+                    variant="secondary"
+                >
                     Login
                 </Button>
+                {hasLoaded && alreadyLoggedInAs && (
+                    <Button
+                        type="submit"
+                        className="mt-3"
+                        variant="default"
+                        onClick={() => router.push('/pit')}
+                    >
+                        Continue as {alreadyLoggedInAs}
+                    </Button>
+                )}
             </div>
         </main>
     );

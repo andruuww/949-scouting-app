3576a60f097e4ee8b95382a4f2b05e3b2bb49ee9
andruuww <0427andrewnguyen@gmail.com>
2023-08-30 14:24:50 -0700

    letmecook pt2

--- worker/index.js
+++ worker/index.js
@@ -1,4 +1,34 @@
+const STATIC_CACHE_NAME = 'site-static';
+const ASSETS = [
+    `/swe-worker-${self.__WB_MANIFEST ? self.__WB_MANIFEST[0].hash : 'development'}.js`,
+    '/icons/manifest-icon-192.maskable.png',
+    '/icons/manifest-icon-512.maskable.png',
+    '/favicon.ico',
+    '/',
+    '/manifest.json',
+    '/pit',
+    '/pit/export',
+    
+];
+
 self.addEventListener('install', evt => {
-    console.log('service worker installed');
     self.__WB_DISABLE_DEV_LOGS = true;
+    // console.log('service worker installed');
+    evt.waitUntil(caches.open(STATIC_CACHE_NAME).then(cache => {
+        console.log('caching page assets');
+        return cache.addAll(ASSETS);
+    }).then(() => {
+        console.log('done caching')
+    }).catch(err => {
+        console.log(ASSETS);
+        console.log('error caching', err);
+    }));
+});
+
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then((response) => {
+            return response || fetch(evt.request);
+        })
+    );
 });

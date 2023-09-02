9df4ca9ca63f71760c0934f01bab593edbe62882
andrew <0427andrewnguyen@gmail.com>
2023-09-02 02:11:24 -0700

    fixed production caching

--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,6 @@
 const PRECACHE_NAME = 'ASSET-PRECACHE-OFFLINE';
 
+// devleopment
 // the array below is crazy sensitive??? so much time went into figuring out this array please don't touch it
 // 16 entries
 // rules of the array
@@ -7,24 +8,35 @@ const PRECACHE_NAME = 'ASSET-PRECACHE-OFFLINE';
 // the page must come before the layout, the main-app comes after both
 // 2) the routes go first, and then the js page files
 // 3) the order of sections should be preserved
-const ASSETS = [
-    // main app
-    '/',
-    '/_next/static/css/app/layout.css',
-    '/_next/static/chunks/app/page.js',
-    '/_next/static/chunks/app/layout.js',
-    '/_next/static/chunks/main-app.js',
-    '/_next/static/chunks/app-pages-internals.js',
+// const ASSETS = [
+//     // main app
+//     '/',
+//     '/_next/static/css/app/layout.css',
+//     '/_next/static/chunks/app/page.js',
+//     '/_next/static/chunks/app/layout.js',
+//     '/_next/static/chunks/main-app.js',
+//     '/_next/static/chunks/app-pages-internals.js',
 
-    // routes
+//     // routes
+//     '/pit',
+//     '/pit/export',
+//     '/_next/static/chunks/app/pit/page.js',
+//     '/_next/static/chunks/app/pit/export/page.js',
+
+//     // other
+//     '',
+//     '/_next/static/chunks/webpack.js',
+//     '/manifest.json',
+//     '/icons/manifest-icon-192.maskable.png',
+//     '/icons/manifest-icon-512.maskable.png',
+//     '/favicon.ico',
+// ];
+
+// production
+const ASSETS = [
     '/pit',
     '/pit/export',
-    '/_next/static/chunks/app/pit/page.js',
-    '/_next/static/chunks/app/pit/export/page.js',
 
-    // other
-    '',
-    '/_next/static/chunks/webpack.js',
     '/manifest.json',
     '/icons/manifest-icon-192.maskable.png',
     '/icons/manifest-icon-512.maskable.png',
@@ -41,9 +53,11 @@ self.addEventListener('install', (evt) => {
                 if (keys.length !== ASSETS.length) {
                     return caches.delete(PRECACHE_NAME).then(() => {
                         console.log('deleted cache');
+
                         return caches
                             .open(PRECACHE_NAME)
                             .then((newCache) => {
+                                console.log('caching');
                                 return newCache.addAll(ASSETS);
                             })
                             .then(() => {
@@ -70,28 +84,3 @@ self.addEventListener('fetch', (event) => {
         })
     );
 });
-
-// debugging code
-// the last asset in the array is the one that might show an error, but might not actually the one that is causing the error
-// let currentAsset = '';
-// let invalidAssets = [];
-// evt.waitUntil(
-//     ASSETS.forEach((asset) => {
-//         caches
-//             .open(PRECACHE_NAME)
-//             .then((cache) => {
-//                 currentAsset = asset;
-//                 return cache.add(asset);
-//             })
-//             .catch((err) => {
-//                 console.log('error caching', err);
-//                 invalidAssets.push(currentAsset);
-//             })
-//             .then(() => {
-//                 console.log('done caching');
-//                 if (invalidAssets.length > 0) {
-//                     console.log('invalid assets', invalidAssets);
-//                 }
-//             });
-//     })
-// );

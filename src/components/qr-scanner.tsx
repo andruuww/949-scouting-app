1bf69f51fbc0ec487c71a4e37ca0a7c652d75e30
andrew nguyen <0427andrewnguyen@gmail.com>
2024-01-19 08:44:08 -0800

    scanner camera sync

--- src/components/qr-scanner.tsx
+++ src/components/qr-scanner.tsx
@@ -6,6 +6,40 @@ import { toast } from 'sonner';
 export default function Scanner({ handleData }: { handleData: (data: string) => void }) {
     const [scanner, setScanner] = React.useState<Html5Qrcode>();
 
+    function startCamera(id: string) {
+        const container = document.getElementById('reader');
+        Html5Qrcode.getCameras()
+            .then(() => {
+                const html5Qrcode = new Html5Qrcode('reader');
+                setScanner(html5Qrcode);
+                return html5Qrcode.start(
+                    { deviceId: id },
+                    {
+                        fps: 30,
+                        disableFlip: true,
+                        aspectRatio: container!.offsetWidth / container!.offsetHeight,
+                    },
+                    (result: string) => {
+                        handleData(result);
+                    },
+                    (error: string) => {}
+                );
+            })
+            .then(() => {
+                const videoElement = container?.querySelector('video');
+                if (videoElement) {
+                    videoElement.style.objectFit = 'cover';
+                    videoElement.style.width = '100%';
+                    videoElement.style.height = '100%';
+                }
+            })
+            .catch((error) => {
+                toast.error('Error', {
+                    description: error.message,
+                });
+            });
+    }
+
     useEffect(() => {
         if (navigator.mediaDevices) {
             const cameras: MediaDeviceInfo[] = [];
@@ -20,41 +54,10 @@ export default function Scanner({ handleData }: { handleData: (data: string) => 
                     // cameras.forEach((camera, i) => {
                     //     toast(`${i}: ${camera.label}`);
                     // });
+                    startCamera(cameras[cameras.length - 1].deviceId);
                 })
                 .catch((error) => {
                     console.log(error);
-                });
-
-            const container = document.getElementById('reader');
-            Html5Qrcode.getCameras()
-                .then(() => {
-                    const html5Qrcode = new Html5Qrcode('reader');
-                    setScanner(html5Qrcode);
-                    return html5Qrcode.start(
-                        { deviceId: cameras[cameras.length - 1]!.deviceId },
-                        {
-                            fps: 30,
-                            disableFlip: true,
-                            aspectRatio: container!.offsetWidth / container!.offsetHeight,
-                        },
-                        (result: string) => {
-                            handleData(result);
-                        },
-                        (error: string) => {}
-                    );
-                })
-                .then(() => {
-                    const videoElement = container?.querySelector('video');
-                    if (videoElement) {
-                        videoElement.style.objectFit = 'cover';
-                        videoElement.style.width = '100%';
-                        videoElement.style.height = '100%';
-                    }
-                })
-                .catch((error) => {
-                    toast.error('Error', {
-                        description: error.message,
-                    });
                 });
         }
     }, [typeof window]);

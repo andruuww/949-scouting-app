fe386ded346066970fdd5c66ddd53ef5873d751e
andrew nguyen <0427andrewnguyen@gmail.com>
2024-01-18 19:12:14 -0800

    new camera selection

--- src/components/qr-scanner.tsx
+++ src/components/qr-scanner.tsx
@@ -6,7 +6,55 @@ import { toast } from 'sonner';
 export default function Scanner({ handleData }: { handleData: (data: string) => void }) {
     const [scanner, setScanner] = React.useState<Html5Qrcode>();
 
-    const QR_PADDING = 50;
+    useEffect(() => {
+        if (navigator.mediaDevices) {
+            const cameras: MediaDeviceInfo[] = [];
+            navigator.mediaDevices
+                .enumerateDevices()
+                .then((devices) => {
+                    devices.forEach((devices) => {
+                        if (devices.kind == 'videoinput') {
+                            cameras.push(devices);
+                        }
+                    });
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
+            const container = document.getElementById('reader');
+            Html5Qrcode.getCameras()
+                .then(() => {
+                    const html5Qrcode = new Html5Qrcode('reader');
+                    setScanner(html5Qrcode);
+                    return html5Qrcode.start(
+                        { deviceId: cameras.at(-1)!.deviceId },
+                        {
+                            fps: 30,
+                            disableFlip: true,
+                            aspectRatio: container!.offsetWidth / container!.offsetHeight,
+                        },
+                        (result: string) => {
+                            handleData(result);
+                        },
+                        (error: string) => {}
+                    );
+                })
+                .then(() => {
+                    const videoElement = container?.querySelector('video');
+                    if (videoElement) {
+                        videoElement.style.objectFit = 'cover';
+                        videoElement.style.width = '100%';
+                        videoElement.style.height = '100%';
+                    }
+                })
+                .catch((error) => {
+                    toast.error('Error', {
+                        description: error.message,
+                    });
+                });
+        }
+    }, [typeof window]);
 
     // stop camera on page leave
     useEffect(() => {
@@ -16,48 +64,6 @@ export default function Scanner({ handleData }: { handleData: (data: string) => 
             } catch {}
         };
     }, [scanner]);
-
-    useEffect(() => {
-        startCamera();
-    }, []);
-
-    function startCamera() {
-        const container = document.getElementById('reader');
-        Html5Qrcode.getCameras()
-            .then(() => {
-                const html5Qrcode = new Html5Qrcode('reader');
-                setScanner(html5Qrcode);
-                return html5Qrcode.start(
-                    { facingMode: 'environment' },
-                    {
-                        fps: 30,
-                        // qrbox: {
-                        //     width: 350,
-                        //     height: 350,
-                        // },
-                        disableFlip: true,
-                        aspectRatio: container!.offsetWidth / container!.offsetHeight,
-                    },
-                    (result: string) => {
-                        handleData(result);
-                    },
-                    (error: string) => {}
-                );
-            })
-            .then(() => {
-                const videoElement = container?.querySelector('video');
-                if (videoElement) {
-                    videoElement.style.objectFit = 'cover';
-                    videoElement.style.width = '100%';
-                    videoElement.style.height = '100%';
-                }
-            })
-            .catch((error) => {
-                toast.error('Error', {
-                    description: error.message,
-                });
-            });
-    }
 
     return (
         <div className='fixed top-0 bottom-0 right-0 left-0 bg-secondary'>

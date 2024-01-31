4bef1fca5ebedd68ed7bd732f20c8d36312375e3
il798li <svpa2008@gmail.com>
2024-01-30 19:21:40 -0800

    finished match scouting
    im pretty sure match scouting is good to go. i havent tested it yet, so not sure if there are any bugs

--- src/jsons/2023/matchscoutingjson.ts
+++ src/jsons/2023/matchscoutingjson.ts
@@ -8,20 +8,16 @@ const matchJSON: JSONFormElement = {
     signature: '%',
     elements: [
         {
-            type: FormElementsType.TITLE,
-            label: 'Autonomous',
-        },
-        {
             type: FormElementsType.GROUP,
             name: 'matchInfo',
             elements: [
                 {
                     label: 'Match Number',
-                    placeholder: '1-64',
+                    placeholder: '64',
                     type: FormElementsType.NUMBER,
                     name: 'matchNumber',
                     required: true,
-                    errorMessage: 'Please provide a match number.,
+                    errorMessage: 'Please provide a match number.'
                 },
                 {
                     label: 'Team Number',
@@ -34,15 +30,50 @@ const matchJSON: JSONFormElement = {
             ],
         },
         {
-            label: 'Robot Position',
+            label: 'Starting Position',
             placeholder: 'Select',
             type: FormElementsType.SELECT,
             name: 'robotPosition',
             options: ['Red 1', 'Red 2', 'Red 3', 'Blue 1', 'Blue 2', 'Blue 3'],
         },
         {
+            type: FormElementsType.TITLE,
+            label: 'Starting'
+        },
+        {
             type: FormElementsType.GROUP,
-            name: 'robotInfo',
+            name: 'startingPosition',
+            elements: [
+                {
+                    label: 'Alliance Color',
+                    placeholder: 'Select',
+                    type: FormElementsType.SELECT,
+                    name: 'color',
+                    options: [
+                        'Red',
+                        'Blue'
+                    ]
+                },
+                {
+                    label: 'Position',
+                    placeholder: 'Select',
+                    type: FormElementsType.SELECT,
+                    name: 'position',
+                    options: [
+                        '[1] Near',
+                        '[2] Middle',
+                        '[3] Far'
+                    ]
+                }
+            ]
+        },
+        {
+            type: FormElementsType.TITLE,
+            label: 'Autonomous',
+        },
+        {
+            type: FormElementsType.GROUP,
+            name: 'autoBonuses',
             elements: [
                 {
                     label: 'Left Starting Zone',
@@ -50,16 +81,16 @@ const matchJSON: JSONFormElement = {
                     name: 'leftStartingZone',
                 },
                 {
-                    label: 'Charge Station Engaged',
+                    label: 'Preloaded Note', //yes ik this isnt a bonus, but it just feels right to put it together with starting zone
                     type: FormElementsType.SWITCH,
-                    name: 'isChargeStationEngaged',
+                    name: 'preloadedNote',
                 },
             ],
         },
         {
-            label: 'Auto Scored Speaker',
+            label: 'Auto: Amp',
             type: FormElementsType.COUNTER,
-            name: 'autoScoredSpeaker',
+            name: 'autoPlayedAmp',
             settings: {
                 increment: 1,
                 min: 0,
@@ -67,9 +98,9 @@ const matchJSON: JSONFormElement = {
             },
         },
         {
-            label: 'Auto Scored Amp',
+            label: 'Auto: Speaker',
             type: FormElementsType.COUNTER,
-            name: 'autoScoredAmp',
+            name: 'Auto Speaker',
             settings: {
                 increment: 1,
                 min: 0,
@@ -81,9 +112,9 @@ const matchJSON: JSONFormElement = {
             label: 'TeleOperated',
         },
         {
-            label: 'TeleOp Scored Speaker',
+            label: 'TeleOp: Amp',
             type: FormElementsType.COUNTER,
-            name: 'teleopScoredSpeaker',
+            name: 'teleOpAmp',
             settings: {
                 increment: 1,
                 min: 0,
@@ -91,9 +122,9 @@ const matchJSON: JSONFormElement = {
             },
         },
         {
-            label: 'TeleOp Scored Amp',
+            label: 'TeleOp: Base Speaker',
             type: FormElementsType.COUNTER,
-            name: 'teleOpScoredAmp',
+            name: 'teleopBaseSpeaker',
             settings: {
                 increment: 1,
                 min: 0,
@@ -101,95 +132,122 @@ const matchJSON: JSONFormElement = {
             },
         },
         {
-            label: 'Missed',
+            label: 'TeleOp: Amplified Speaker',
             type: FormElementsType.COUNTER,
-            name: 'missed',
+            name: 'teleopAmplifiedSpeaker',
+            settings: {
+                increment: 1,
+                min: 0,
+                max: 100,
+            }
+        },
+        {
+            label: 'TeleOp: Missed Speaker',
+            type: FormElementsType.COUNTER,
+            name: 'missedSpeaker',
             settings: {
                 increment: 1,
                 min: 0,
                 max: 100,
             },
+        },
+        {
+            label: 'Coopertition',
+            type: FormElementsType.SWITCH,
+            name: 'coopertition',
+        },
+        {
+            label: 'Fouls',
+            type: FormElementsType.TITLE,
+        },
+        {
+            label: 'Foul',
+            type: FormElementsType.COUNTER,
+            settings: {
+                increment: 1,
+                min: 0,
+                max: 100,
+            },
+            name: 'regularFoul'
+        },
+        {
+            label: 'Tech Foul',
+            type: FormElementsType.COUNTER,
+            settings: {
+                increment: 1,
+                min: 0,
+                max: 100,
+            },
+            name: 'techFoul'
+        },
+        {
+            label: 'Yellow Card',
+            type: FormElementsType.SWITCH,
+            name: 'yellowCard'
+        },
+        {
+            label: 'Red Card', // results in disqualification
+            type: FormElementsType.SWITCH,
+            name: 'redCard'
+        },
+        {
+            label: 'Disabled',
+            type: FormElementsType.SWITCH,
+            name: 'Ddsabled',
+        },
+        {
+            label: 'Endgame',
+            type: FormElementsType.TITLE,
+            name: 'trap'
+        },
+        {
+            label: 'End Position',
+            type: FormElementsType.SELECT,
+            options: ['None', 'Podium', 'Stage']
+        },
+        {
+            label: 'Trap',
+            type: FormElementsType.SWITCH,
+            name: 'Trap', // robot scores note into the goal thats inside the podium. based on animation, i THINK you have to be onstage to do this
+        },
+        {
+            label: 'Harmony',
+            type: FormElementsType.SWITCH, //max of 2 robots can fit on one chain, so max 1 harmony point, so i used switch instead of counter
+        },
+        {
+            label: 'Microphone',
+            type: FormElementsType.SWITCH,
+            name: 'microphone',
+        },
+        {
+            label: 'Overall Notes',
+            type: FormElementsType.TITLE,
         },
         {
             label: 'Playstyle',
             placeholder: 'Select Playstyle',
             type: FormElementsType.SELECT,
             name: 'playstyle',
-            options: ['None', 'Offensive', 'Defensive', 'Balanced'],
+            options: ['None', 'Offensive', 'Defensive', 'Balanced'], 
+            // would recommend removing the None option. this would force scouters to try their best to pick one of the other three.
+            // also we could have a "Wild" style to replace None in case the robot was way too unpredictable
         },
         {
-            label: 'Overall Match Notes',
-            type: FormElementsType.TITLE,
-        },
-        {
-            label: 'Yellow Card',
+            label: 'Break Down',
             type: FormElementsType.SWITCH,
-            name: 'yellowCard',
-        },
-        {
-            label: 'Broke Down',
-            type: FormElementsType.SWITCH,
-            name: 'brokeDown',
+            name: 'breakDown',
         },
         {
             type: FormElementsType.TEXTAREA,
             name: 'breakdownDescription',
-            placeholder: 'Describe the breakdown',
-            required: false,
-        },
-        {
-            label: 'Links',
-            type: FormElementsType.COUNTER,
-            name: 'links',
-            settings: {
-                increment: 1,
-                min: 0,
-                max: 100,
-            },
-        },
-        {
-            label: 'Fouls',
-            type: FormElementsType.COUNTER,
-            name: 'fouls',
-            settings: {
-                increment: 5,
-                min: 0,
-                max: 100,
-            },
-        },
-        {
-            label: 'Tech Fouls',
-            type: FormElementsType.COUNTER,
-            name: 'techFouls',
-            settings: {
-                increment: 12,
-                min: 0,
-                max: 100,
-            },
-        },
-
-        {
-            label: 'Charge Station Points',
-            type: FormElementsType.COUNTER,
-            name: 'chargeStationPoints',
-            settings: {
-                increment: 10,
-                min: 0,
-                max: 30,
-            },
-        },
-        {
-            label: 'Bonuses',
-            type: FormElementsType.CHECKBOX,
-            name: 'bonuses',
-            options: ['Sustainability', 'Cooperation', 'Activation'],
+            placeholder: 'Describe how the robot broke down. ',
             required: false,
         },
         {
             label: 'Additional Notes',
             type: FormElementsType.TEXTAREA,
             name: 'additionalNotes',
-            placeholder: 'Enter any additional notes here',
+            placeholder: 'Enter any additional notes here.',
             required: false,
         },
         {
